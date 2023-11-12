f4c0073760e2fb6e679caebe126248bc40a2d88c
Annie <anniebui0110@gmil.com>
2023-11-12 12:28:17 -0600

    add middleware to auth.ks

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,20 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'mysecretssshhhhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req) {
-    // allows token to be sent via  req.query or headers
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // We split the token string into an array and return actual token
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -19,7 +23,7 @@ module.exports = {
       return req;
     }
 
-    // verify token and get user data out of it
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -27,12 +31,11 @@ module.exports = {
       console.log('Invalid token');
     }
 
-    // send to next endpoint
+    // return the request object so it can be passed to the resolver as `context`
     return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
