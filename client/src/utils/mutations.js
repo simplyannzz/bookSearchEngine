2175362282af48843da07ec5432899f4155000da
Annie <anniebui0110@gmil.com>
2023-11-13 21:26:55 -0600

    mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,10 @@
-import {ggl} from "apollo/client";
+import { ggl } from "apollo/client";
 
+export const LOGIN_USER = 'ggl'
+mutation 
+export const ADD_USER = 'ggl'
+
+
+export const SAVE_BOOK = 'ggl'
+
+export const REMOVE_BOOK = 'ggl'
